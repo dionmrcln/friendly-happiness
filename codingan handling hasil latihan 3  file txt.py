c3f267e1850ef800395ed846027a794eb642ee5d
dionmrcln <97133563+dionmrcln@users.noreply.github.com>
2022-01-05 06:10:20 +0700

    Add files via upload

---
+++ codingan handling hasil latihan 3  file txt.py
@@ -0,0 +1,12 @@
+#kita lagi belajar file handling mode add
+dataqu = open ('latihan 3.txt', 'a')
+#menambahkan kalimat di akhir
+dataqu.write("\nLaba bulan ke-  1 Sebesar:  0")
+dataqu.write("\nLaba bulan ke-  2 Sebesar:  0")
+dataqu.write("\nLaba bulan ke-  3 sebesar:  10000000.0")
+dataqu.write("\nlaba bulan ke-  4 sebesar:  10000000.0")
+dataqu.write("\nLaba bulan ke-  5 sebesar:  50000000.0")
+dataqu.write("\nLaba bulan ke-  6 sebesar:  50000000.0")
+dataqu.write("\nLaba bulan ke-  7 sebesar:  50000000.0")
+dataqu.write("\nLaba bulan ke-  8 sebesar:  20000000.0")
+dataqu.close()
---
+++ latihan 3.py
@@ -0,0 +1,17 @@
+a = 100000000
+for i in range(1, 9):
+    if (i >= 1 and i <= 2):
+        b = a * 0
+        print("Laba bulan ke- ", i, "Sebesar: ", b)
+    if (i >= 3 and i <= 4):
+        c = a * 0.1
+        print("Laba bulan ke- ", i, "sebesar: ", c)
+    if (i >= 5 and i <= 7):
+        d = a * 0.5
+        print("Laba bulan ke- ", i, "sebesar: ", d)
+    if (i == 8):
+        e = a * 0.2
+        print("Laba bulan ke- ", i, "sebesar: ", e)
+        total = b + b + c + c + d + d + d + e
+        print = ("\nTotal : ", total)
+        
